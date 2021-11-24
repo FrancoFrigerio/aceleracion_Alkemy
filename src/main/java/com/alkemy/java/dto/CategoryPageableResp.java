3d97422ddb4247f23d8ae9cd9b65bea75f28372e
FRANCO <franco.p.f@live.com.ar>
2021-11-24 14:53:11 -0300

    add category

---
+++ src/main/java/com/alkemy/java/dto/CategoryPageableResp.java
@@ -0,0 +1,14 @@
+package com.alkemy.java.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CategoryPageableResp {
+
+    private String name;
+}
